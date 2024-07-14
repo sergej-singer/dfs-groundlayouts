3b4ba397b32dd80f96d1d58732ec6968c09a0a58
Sergei Dutenhefner <74149595+sergej-singer@users.noreply.github.com>
2024-07-14 23:14:51 +0200

    EDDL DFS Layout
    De-Icing Areas, APP Window

--- mapbuilder/mapbuilder/utils/ecl.py
+++ mapbuilder/mapbuilder/utils/ecl.py
@@ -1,7 +1,6 @@
 
 from mapbuilder.utils.geo import Brg, Fix, back_bearing
 
-
 def extrapolate_rwy(rwy_info, count: int, dist: float = 1) -> list[Fix]:
     """Extrapolates count points from the runway threshold, each spaced dist nautical miles apart
     from each other."""
@@ -34,14 +33,19 @@ def draw_loc_tick(rwy_info, gap: float, length: float):
     return str(Fix(rwy_info["loc"]).move_to(gap, brg).line_to(length, brg))
 
 
-def draw_marker_ticks(rwy_info, at: list, gap: float, length: float):
+def draw_marker_ticks(rwy_info, at: list, gap: float, length: float, side: int = 0):
     lines = []
     brg = Brg(rwy_info["bearing"]).invert()
     tick_brg = brg + 90
     for dist in at:
         base = Fix(rwy_info["thr"]).move_to(dist, brg)
 
-        lines.extend((str(base.move_to(gap, tick_brg).line_to(length, tick_brg)),
-                      str(base.move_to(-gap, tick_brg).line_to(-length, tick_brg))))
+        if side == 0:
+            lines.extend((str(base.move_to(gap, tick_brg).line_to(length, tick_brg)),
+                        str(base.move_to(-gap, tick_brg).line_to(-length, tick_brg))))
+        elif side == 1:
+            lines.append(str(base.move_to(gap, tick_brg).line_to(length, tick_brg)))
+        else:
+            lines.append(str(base.move_to(-gap, tick_brg).line_to(-length, tick_brg)))
 
     return "\n".join(lines)
